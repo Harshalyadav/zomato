6584c964b9c0efb9bd15317fe8e269a8d209b388
pavanleton <pavankumarkpkp@gmail.com>
2021-09-16 14:08:57 +0530

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh "ubuntu@65.1.129.250" "cd app/zomato-clone-2 && docker-compose stop && git pull origin master && docker-compose up -d --build && exit"
+ssh "ubuntu@65.1.129.250" "cd app/zomato-clone-2 && sudo docker-compose stop && git pull origin master && sudo docker-compose up -d --build && exit"
